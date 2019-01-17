e3c966fbc38577f84ffc0ca96ffc8ed0ff72fdbd
Phillip Schleicher <schleicher@nexus-netsoft.com>
2019-01-17 22:41:44 +0100

    Removed plugin

--- plugins/DeployHistory/install/install.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `deployee_exec_history` (
-  `name` VARCHAR(255) NOT NULL,
-  `deploytime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `success` TINYINT(1) UNSIGNED NOT NULL,
-  INDEX `name_success` (`name`, `success`)
-)
-  COLLATE='utf8_general_ci'
-  ENGINE=InnoDB
-;
