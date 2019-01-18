cd84b4914349ea35197659accdf2ea86cfffd114
Phillip Schleicher <schleicher@nexus-netsoft.com>
2019-01-18 20:15:31 +0100

    Refactored dispatcher

--- plugins/ShopwareTasks/Dispatcher/CreateAdminUserDispatcher.php
+++ plugins/ShopwareTasks/Dispatcher/CreateAdminUserDispatcher.php
@@ -5,7 +5,6 @@ namespace Deployee\Plugins\ShopwareTasks\Dispatcher;
 use Deployee\Plugins\Deploy\Definitions\Tasks\TaskDefinitionInterface;
 use Deployee\Plugins\Deploy\Dispatcher\AbstractTaskDefinitionDispatcher;
 use Deployee\Plugins\Deploy\Dispatcher\DispatchResultInterface;
-use Deployee\Plugins\ShellTasks\Definitions\ShellTaskDefinition;
 use Deployee\Plugins\ShopwareTasks\Definitions\CreateAdminUserDefinition;
 use Deployee\Plugins\ShopwareTasks\Definitions\ShopwareCommandDefinition;
 
@@ -20,23 +19,27 @@ class CreateAdminUserDispatcher extends AbstractTaskDefinitionDispatcher
         return $taskDefinition instanceof CreateAdminUserDefinition;
     }
 
-
+    /**
+     * @param TaskDefinitionInterface $taskDefinition
+     * @return DispatchResultInterface
+     * @throws \Deployee\Plugins\Deploy\Exception\DispatcherException
+     */
     public function dispatch(TaskDefinitionInterface $taskDefinition): DispatchResultInterface
     {
         $parameter = $taskDefinition->define();
-        $swCommand = new ShopwareCommandDefinition(
-            'sw:admin:create',
-            sprintf(
-                "--username=%s --password=%s --email=%s --name=%s --locale=%s -n",
-                escapeshellarg($parameter->get('username')),
-                escapeshellarg($parameter->get('password')),
-                escapeshellarg($parameter->get('email')),
-                escapeshellarg($parameter->get('name')),
-                escapeshellarg($parameter->get('locale'))
+        return $this->delegate(
+            new ShopwareCommandDefinition(
+                'sw:admin:create',
+                sprintf(
+                    "--username=%s --password=%s --email=%s --name=%s --locale=%s -n",
+                    escapeshellarg($parameter->get('username')),
+                    escapeshellarg($parameter->get('password')),
+                    escapeshellarg($parameter->get('email')),
+                    escapeshellarg($parameter->get('name')),
+                    escapeshellarg($parameter->get('locale'))
+                )
             )
         );
-
-        return $this->delegate($swCommand);
     }
 
 }
